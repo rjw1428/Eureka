2058ddf9dc7069bf821d1c54c4ce3b76eecad088
Ryan Wilk <software@ryanwilk.com>
2024-11-18 22:33:20 -0500

    [Rxjs] WindowToggle

--- rxjs/window.ts
+++ rxjs/window.ts
@@ -1,6 +1,10 @@
-import { interval, windowTime, map, toArray, switchMap, takeWhile } from 'rxjs';
+import { interval, windowTime, map, toArray, switchMap, takeWhile, tap, of, take, windowToggle, timer } from 'rxjs';
 
-const source = interval(1000); // Simulate stock price updates every second
+// Simulate some event occurring every second
+const source = of('').pipe( 
+    tap(() => console.log('START')),
+    switchMap(() => interval(1000)) 
+);
 
 const example = source.pipe(
     windowTime(5000), // Create 5-second windows
@@ -16,7 +20,7 @@ const example = source.pipe(
     takeWhile((val) => val.max < 25)
 );
 
-example.subscribe(val => console.log(val));
+// example.subscribe(val => console.log(val));
 
 /*
 Will emit the following, one every 5 seconds
@@ -29,4 +33,25 @@ Will emit the following, one every 5 seconds
 then will wait 5 seconds to "get" the next state
     { average: 26, max: 28, min: 24 }
 and since that fails the `takeWhile`, it completes
+ */
+
+const OPEN = 5000
+const CLOSE = 3000
+const example2 = source.pipe(
+    windowToggle(interval(OPEN), () => interval(CLOSE)),
+    switchMap(window$ => window$.pipe(toArray())), // Flatten the window observable
+    take(3)
+);
+
+example2.subscribe(console.log);
+/*
+using `windowToggle`, we ignore the source value until the window opens.
+once then, we continue accumulating until the window closes.
+If the window open time is too short, that the close never occurs, then no value get emitted
+
+resulting in:
+wait 5 seconds for the window to open
+[ 4, 5, 6 ] // after 3 seconds close & accumulate
+[ 9, 10, 11 ] // next 5 second trigger starts, 3 seconds later we close
+[ 14, 15, 16 ]
  */
