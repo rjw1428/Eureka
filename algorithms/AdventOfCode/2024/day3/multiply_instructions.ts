d6d2385190cd1423866a2dcbe1faa89d67ee85f3
Ryan Wilk <software@ryanwilk.com>
2024-12-03 22:40:08 -0500

    [AdventOfCode] Day 3 - Part 2

--- algorithms/AdventOfCode/2024/day3/multiply_instructions.ts
+++ algorithms/AdventOfCode/2024/day3/multiply_instructions.ts
@@ -16,6 +16,7 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
 
 Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+174336360
 */
 
 import fs from "node:fs";
@@ -25,7 +26,9 @@ fs.readFile("input.txt", "utf8", (err, data) => {
         console.error(err);
         return;
     }
-    console.log(multiplyData(data))
+
+    const filteredData = filterData(data)
+    console.log(multiplyData(filteredData))
 });
 
 function multiplyData(data: string, total: number = 0): number {
@@ -51,3 +54,45 @@ function multiplyData(data: string, total: number = 0): number {
     return multiplyData(data.substring(end + 1), total)
 }
 
+
+/* 
+--- Part Two ---
+As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.
+
+There are two new instructions you'll need to handle:
+
+The do() instruction enables future mul instructions.
+The don't() instruction disables future mul instructions.
+Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+For example:
+
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+
+Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+88802350
+*/
+
+function filterData(data: string) {
+    let pointer = 0
+    let result = data
+    while (pointer < data.length) {
+        const start = result.indexOf('don\'t()')
+        const end = result.substring(start).indexOf('do()')
+        if (start < 0) {
+            break
+        }
+        if (end >= 0) {
+            console.log(start, start + end)
+            result = result.substring(0, start) + result.substring(start + end)
+            pointer = start + end + 1
+        } else {
+            result = result.substring(0, start)
+            pointer = data.length
+        }
+    }
+    return result
+}
