e878451e14b7121320c1284eefe7ea5f53d4cc8b
Ryan Wilk <software@ryanwilk.com>
2025-03-05 07:03:02 -0500

    [DevEx] Created new domain

---
+++ devEx/lint/index.js
@@ -0,0 +1,23 @@
+function test() {
+    console.log("here");
+}
+
+function getAuthenticationMessage(isAuthenticated) {
+    return isAuthenticated
+        ? "User is authenticated"
+        : "User is not authenticated";
+}
+
+function oldMethodName() {
+    return "doesn't do anything";
+}
+
+function newMethodName() {
+    return "replaced method";
+}
+
+function test2() {
+    newMethodName();
+}
+
+test2();
---
+++ devEx/lint/plugins/tests/single-line-imports.js
@@ -0,0 +1,57 @@
+// plugin/tests/single-line-import.test.js
+const { RuleTester } = require("eslint");
+const rule = require("../rules/single-line-import");
+
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+    },
+});
+
+ruleTester.run("single-line-import", rule, {
+    valid: [
+        {
+            code: "import { foo, bar, baz } from 'module';",
+        },
+        {
+            code: "import defaultExport, { foo, bar, baz as renamed } from 'module';",
+        },
+        {
+            code: "import * as name from 'module';",
+        },
+        {
+            code: "import 'module';",
+        },
+    ],
+    invalid: [
+        {
+            code: `import {
+                        foo,
+                        bar,
+                        baz
+                    } from 'module';`,
+            output: "import { foo, bar, baz } from 'module';",
+            errors: [{ message: "Import statements must be on a single line" }],
+        },
+        {
+            code: `import defaultExport, {
+                        foo,
+                        bar,
+                        baz as renamed
+                    } from 'module';`,
+            output: "import defaultExport, { foo, bar, baz as renamed } from 'module';",
+            errors: [{ message: "Import statements must be on a single line" }],
+        },
+        {
+            code: `import {
+                        reallyLongImportName1,
+                        reallyLongImportName2,
+                        reallyLongImportName3,
+                        reallyLongImportName4
+                    } from 'really-long-module-name';`,
+            output: "import { reallyLongImportName1, reallyLongImportName2, reallyLongImportName3, reallyLongImportName4 } from 'really-long-module-name';",
+            errors: [{ message: "Import statements must be on a single line" }],
+        },
+    ],
+});
